3d5e3ed6f97f7bfdb8694fe50cab42e0ae588a53
DNS <ivanov2161@gmail.com>
2022-12-08 17:20:08 +0400

    forms.py added CSS_CLASS constant

--- mysite/goen/forms.py
+++ mysite/goen/forms.py
@@ -2,19 +2,21 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.forms import ModelForm
+from .models import Book
 
-from .models import *
+CSS_CLASS = 'form-input u-radius-6 u-border-1 u-border-grey-30 u-input u-input-rectangle'
 
 
 class Answer(forms.Form):
-    answer = forms.CharField(max_length=128, label='', widget=forms.TextInput(attrs={'autofocus': True, 'class':'form-input u-radius-6 u-border-1 u-border-grey-30 u-input u-input-rectangle'}))
+    answer = forms.CharField(max_length=128, label='',
+                             widget=forms.TextInput(attrs={'autofocus': True, 'class': CSS_CLASS}))
 
 
 class RegisterUserForm(UserCreationForm):
-    username = forms.CharField(label='USER NAME', widget=forms.TextInput(attrs={'class': 'form-input u-radius-6 u-border-1 u-border-grey-30 u-input u-input-rectangle'}))
-    email = forms.EmailField(label='E-MAIL', widget=forms.EmailInput(attrs={'class': 'form-input u-radius-6 u-border-1 u-border-grey-30 u-input u-input-rectangle'}))
-    password1 = forms.CharField(label='PASSWORD', widget=forms.PasswordInput(attrs={'class': 'form-input u-radius-6 u-border-1 u-border-grey-30 u-input u-input-rectangle'}))
-    password2 = forms.CharField(label='REPEAT PASSWORD', widget=forms.PasswordInput(attrs={'class': 'form-input u-radius-6 u-border-1 u-border-grey-30 u-input u-input-rectangle'}))
+    username = forms.CharField(label='USER NAME', widget=forms.TextInput(attrs={'class': CSS_CLASS}))
+    email = forms.EmailField(label='E-MAIL', widget=forms.EmailInput(attrs={'class': CSS_CLASS}))
+    password1 = forms.CharField(label='PASSWORD', widget=forms.PasswordInput(attrs={'class': CSS_CLASS}))
+    password2 = forms.CharField(label='REPEAT PASSWORD', widget=forms.PasswordInput(attrs={'class': CSS_CLASS}))
 
     class Meta:
         model = User
@@ -22,14 +24,14 @@ class RegisterUserForm(UserCreationForm):
 
 
 class LoginUserForm(AuthenticationForm):
-    username = forms.CharField(label='USER NAME', widget=forms.TextInput(attrs={'class': 'form-input u-radius-6 u-border-1 u-border-grey-30 u-input u-input-rectangle'}))
-    password = forms.CharField(label='PASSWORD', widget=forms.PasswordInput(attrs={'class': 'form-input u-radius-6 u-border-1 u-border-grey-30 u-input u-input-rectangle'}))
+    username = forms.CharField(label='USER NAME', widget=forms.TextInput(attrs={'class': CSS_CLASS}))
+    password = forms.CharField(label='PASSWORD', widget=forms.PasswordInput(attrs={'class': CSS_CLASS}))
 
 
 class UploadBook(ModelForm):
-    name = forms.CharField(label='TITLE', widget=forms.TextInput(attrs={'class': 'form-input u-radius-6 u-border-1 u-border-grey-30 u-input u-input-rectangle'}))
-    wholeText = forms.CharField(label='Text', widget=forms.Textarea(attrs={'class': 'form-input u-radius-6 u-border-1 u-border-grey-30 u-input u-input-rectangle'}))
-    cover = forms.ImageField(label='cover')
+    name = forms.CharField(label='TITLE', widget=forms.TextInput(attrs={'class': CSS_CLASS}))
+    wholeText = forms.CharField(label='Text', widget=forms.Textarea(attrs={'class': CSS_CLASS}))
+    cover = forms.ImageField(label='cover', widget=forms.ClearableFileInput)
 
     class Meta:
         model = Book
