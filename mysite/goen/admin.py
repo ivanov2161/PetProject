4cdd05f9c0d9047762a43cd520eb5c6645437adf
DNS <ivanov2161@gmail.com>
2023-01-09 10:30:58 +0400

    Admin page changes
    Added list_displays and search_fields to admin page for usefully

--- mysite/goen/admin.py
+++ mysite/goen/admin.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 
 from .models import Story, Word, WordLearned
 
-admin.site.register(Story)
-admin.site.register(Word)
-admin.site.register(WordLearned)
+
+class StoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'upload_date']
+
+
+class WordAdmin(admin.ModelAdmin):
+    list_display = ['word_original', 'word_translate', 'word_description', 'story']
+    search_fields = ['word_original']
+
+
+class WordLearnedAdmin(admin.ModelAdmin):
+    list_display = ['learn_word', 'learn_person', 'progress', 'is_learned', 'next_day_learn']
+    search_fields = ['learn_word__word_original', 'learn_person', 'progress', 'is_learned']
+
+
+admin.site.register(Story, StoryAdmin)
+admin.site.register(Word, WordAdmin)
+admin.site.register(WordLearned, WordLearnedAdmin)
